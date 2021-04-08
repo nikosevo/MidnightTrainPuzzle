a7d375a49b6ce8e515078ff4262a8b6cf6e3712e
nikos <nikos.evo@hotmail.com>
2021-04-08 14:56:02 +0200

    delete Node and add it to main

---
+++ main.py
@@ -0,0 +1,23 @@
+class Node:
+    def __init__(self,name,parent,state,deapth,cost,counter):
+        self.name = name
+        self.parent = parent
+        self.state = state
+        self.deapth = deapth
+        self.cost = cost
+        self.counter = counter + 1
+
+    @property
+    def checkForFinalState(self):
+        if self.state == finalState:
+            return True
+        return False
+
+
+
+finalState =    {True,True,True}
+currentState =  {False,False,False}
+costs =         {  1,    3,    6}
+
+nodes_created = 0
+nodes_visited = 0
