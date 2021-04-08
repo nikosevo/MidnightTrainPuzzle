ca64f67f4f9bede5d9a47dd62aed079a47b9e771
Valantis Pateiniotis <balantispat@gmail.com>
2021-04-08 15:34:24 +0300

    usc upload

--- USC_Algo.py
+++ USC_Algo.py
@@ -1,29 +1,11 @@
-#start with the class and a two way constructor one for the starting node and one for the other nodes
-class Node:
-    def __init__(self, state, args,action,totalcost,totaldepth):
-        if isinstance(args,Node):
-            self.state = state
-            self.args = Node
-            self.action = action
-            self.totalcost = totalcost
-            self.totaldepth = totaldepth
-        if args == None:
-            self.state = state
-            self.args = None
-            self.action = action
-            self.totalcost = totalcost
-            self.totaldepth = totaldepth
-            
 
-#This can be changed later as to how we will calculate the cost and the depth
-prevcost=0
-newcost=0
-prevdepth=0
-newdepth=0
+def Ucs:
+    this.treeNode.add(new Node("starting",this,null,this.state,0,0))
+    while treeNode !=None:
+        this.visits=this.visits+1
+        print(visits)
 
-#initialized states later to be made by a def function
-startingstate=[["A",2,False],["A1",3,False]]
-action=[["A",2,False],["A1",3,True]]
-
-startignNode = Node(startingstate,None,action,prevcost+newcost,prevdepth+newdepth)
-print(startignNode)
+        Node minCostNode = None
+        mincost=0 #this will need to become as big as possible Max_Value
+        for i in range(treeNode.size()):
+            if this.treeNode
